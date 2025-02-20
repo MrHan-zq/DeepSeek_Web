19a52c5ac1df0bda6e02e3ec8fe92b2a5320848e
ext.hanjun <ext.hanjun@jd.com>
2025-02-20 20:25:47 +0800

    增加依赖

--- src/main/java/com/example/deepseek/enums/HttpEnum.java
+++ src/main/java/com/example/deepseek/enums/HttpEnum.java
@@ -11,7 +11,13 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum HttpEnum {
     SUCCESS(200, "操作成功"),
-    FAIL(-1, "操作失败");
-    private int code;
-    private String message;
+    FAIL(500, "操作失败"),
+    PARAM_ERROR(101, "参数错误"),
+    NO_AUTH(102, "无权限"),
+//    SYSTEM_ERROR(500, "系统异常"),
+    NO_DATA(104, "无数据"),
+    BUSINESS_ERROR(105, "业务错误"),
+    DB_ERROR(106, "数据库错误");
+    private final int code;
+    private final String message;
 }
